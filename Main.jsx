a27935f6a2fc70260053ae8e8b595f3eed5d262e
TechSekani <116392903+TechSekani@users.noreply.github.com>
2023-11-05 02:58:10 +0100

    added the services component

---
+++ Main.jsx
@@ -0,0 +1,8 @@
+// const changeNavbarColor = () => {
+//     if (window.scrollY >= 80) {
+//       setColorchange(true);
+//     } else {
+//       setColorchange(false);
+//     }
+//   };
+//   window.addEventListener("scroll", changeNavbarColor);
